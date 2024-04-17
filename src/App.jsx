e2d7bccb3c93380c386e3f58b5f0e8cc7518a0df
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-17 18:51:31 -0500

    feat: :sparkles: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from './sections/Header.jsx'
+
 function App() {
   return (
-    <div className="bg-red-500">
-      ga1
+    <div className="bg-[#202529]/95 text-white">
+      <Header/>
     </div>
   )
 }
