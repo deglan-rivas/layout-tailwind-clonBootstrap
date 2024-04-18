cf7c7feed80370c6b97d71f041a3e2505ddc6625
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-17 19:40:24 -0500

    feat: :sparkles: add main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './sections/Header.jsx'
+import Main from './sections/Main.jsx'
 
 function App() {
   return (
     <div className="bg-[#202529]/95 text-white">
       <Header/>
+      <Main/>
     </div>
   )
 }
