9d1d0689722ac6d19943ef53a2738fd055e31fc6
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-17 19:46:12 -0500

    build: :heavy_plus_sign: add code highlighter dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import GetStarted from './sections/GetStarted.jsx'
 import Header from './sections/Header.jsx'
 import Main from './sections/Main.jsx'
 
@@ -6,6 +7,7 @@ function App() {
     <div className="bg-[#202529]/95 text-white">
       <Header/>
       <Main/>
+      <GetStarted/>
     </div>
   )
 }
